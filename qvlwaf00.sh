5e66c3260865f91b740b75343595f9ccaacf2bdc
Marcos Pablo Russo <marcospr1974@gmail.com>
2017-01-11 19:18:01 +0000

    Script para levantar el container waf

--- qvlwaf00.sh
+++ qvlwaf00.sh
@@ -4,6 +4,5 @@ docker run -d \
   --link qvlnag00 \
   --hostname qvlwaf00 \
   --name qvlwaf00 \
-  -p 80:80 \
-  -v /opt/docker/qvlwaf00/etc/apache2:/etc/apache2 \
+  -p 8081:80 \
   marcospr1974/alpine-waf

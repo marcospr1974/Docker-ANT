ad10cfdbff2d53514f0f3dce6294efcc34a228ec
Marcos Pablo Russo <marcospr1974@gmail.com>
2017-01-12 19:54:01 +0000

    Mejoras en el Dockerfile de alpine-haproxy

---
+++ alpine-haproxy/start.sh
@@ -0,0 +1,15 @@
+#!/bin/sh 
+
+/usr/sbin/ucarp --interface=${UCARP_INTERFACE} \
+                --srcip=${UCARP_SOURCEADDRESS} \
+                --vhid=${UCARP_VHID} \
+                --pass=${UCARP_PASS} \
+                --addr=${UCARP_VIRTUALADDRESS} \
+                --upscript=${UCARP_UPSCRIPT} \
+                --downscript=${UCARP_DOWNSCRIPT} \
+                --xparam=${UCARP_VIRTUALPREFIX} \
+                --nomcast \
+                ${UCARP_OPTS}
+
+
+haproxy -d -f /etc/haproxy/haproxy.cfg

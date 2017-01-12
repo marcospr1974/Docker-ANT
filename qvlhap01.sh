95e00fc643a11e333d7427f145a2e06d5ddd126c
Marcos Pablo Russo <marcospr1974@gmail.com>
2017-01-12 20:36:28 +0000

    Se agrego --privileged porque no permitia agregar la ip virtual

--- qvlhap01.sh
+++ qvlhap01.sh
@@ -4,6 +4,7 @@ docker run -d \
   --hostname qvlhap01 \
   --name qvlhap01 \
   -p 8090:80 \
+  --privileged \
   -e UCARP_SOURCEADDRESS=172.17.0.19 \
   -v /opt/docker/qvlhap00/etc/haproxy:/etc/haproxy \
   marcospr1974/alpine-haproxy 

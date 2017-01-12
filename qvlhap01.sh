1b81c89b2a00e9722abd7a77cd1c3d8997e96efb
Marcos Pablo Russo <marcospr1974@gmail.com>
2017-01-12 19:54:21 +0000

    Run container qvlhap01.sh

---
+++ qvlhap01.sh
@@ -0,0 +1,9 @@
+docker run -d \
+  --link qvlwaf00 \
+  --link qvlwaf01 \
+  --hostname qvlhap01 \
+  --name qvlhap01 \
+  -p 8090:80 \
+  -e UCARP_SOURCEADDRESS=172.17.0.19 \
+  -v /opt/docker/qvlhap00/etc/haproxy:/etc/haproxy \
+  marcospr1974/alpine-haproxy 

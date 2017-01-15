8623e3227700707a621953c87a049e3e14fb9221
root <root@virtual.my.domain>
2017-01-15 18:22:24 +0000

    Script para ejecutar los container

--- run_container.sh
+++ run_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-container="qvldns00 qvlgit00 qvldba00 qvlnag00 qvlweb00 qvljoo00 qvlwaf00 qvlwaf01 qvlhap00"
+container="qvldns00 qvlgit00 qvldba00 qvlnag00 qvlweb00 qvljoo00 qvlwaf00 qvlhap00"
 for CONTAINER in ${container}
 do
    docker start ${CONTAINER}

a6ad1e4205cc8c888f1dd78dec9edd0ccd724c8f
root <root@virtual.my.domain>
2017-01-15 20:16:54 +0000

    Mejoras script container qvljoo00

--- qvljoo00.sh
+++ qvljoo00.sh
@@ -2,5 +2,6 @@ docker run -d \
   --link qvldba00 \
   --hostname qvljoo00 \
   --name qvljoo00 \
+  -v /opt/docker/qvljoo00/var/www/localhost/htdocs:/var/www/localhost/htdocs \
   -p 8089:80 \
-  marcospr1974/alpine-joomla
+  marcospr1974/alpine-apache2

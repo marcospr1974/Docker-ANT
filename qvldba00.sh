ea547af30932e04d965cc6cd0ec385c5c36e13c2
root <root@virtual.my.domain>
2017-01-15 20:16:45 +0000

    Mejoras script container qvldba00

--- qvldba00.sh
+++ qvldba00.sh
@@ -6,5 +6,5 @@ docker run -d \
   -e MYSQL_ROOT_PASSWORD='debian' \
   -v /opt/docker/qvldba00/var/lib/mysql:/var/lib/mysql \
   -v /opt/docker/qvldba00/etc/mysql:/etc/mysql \
-  -p 3306 \
+  -p 3306:3306 \
   marcospr1974/alpine-mariadb
